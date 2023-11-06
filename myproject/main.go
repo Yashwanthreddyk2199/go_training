8d229b60630f2aa770c346ba003ddc04ce6caf1a
Yashwanthreddyk2199 <yashwanth.k2199@gmail.com>
2023-11-06 16:29:40 -0500

    first commit

---
+++ myproject/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	//"myproject/simplecalc"
+)
+
+func main() {
+	//a, b := 12, 3
+
+
+	//fmt.Println(simplecalc.Add(a, b))
+	//fmt.Println(simplecalc.Sub(a, b))
+	//fmt.Println(simplecalc.Mul(a, b))
+	fmt.Println("Hello")
+}
