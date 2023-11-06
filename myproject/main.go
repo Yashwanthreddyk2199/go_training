ab2c36f54fe8bb4bd3858498b042785a9d99db34
Yashwanthreddyk2199 <yashwanth.k2199@gmail.com>
2023-11-06 16:43:53 -0500

    for loop and simplecalc

--- myproject/main.go
+++ myproject/main.go
@@ -1,16 +1,22 @@
-package main
+package main // always starts with
 
 import (
-	"fmt"
-	//"myproject/simplecalc"
+	"fmt" // contains all the basic functions
+	//"myproject/simplecalc" // error in linking shows not in the src directory
 )
 
 func main() {
-	//a, b := 12, 3
-
-
+	//a, b := 12, 3 // variable declaratrion
 	//fmt.Println(simplecalc.Add(a, b))
 	//fmt.Println(simplecalc.Sub(a, b))
 	//fmt.Println(simplecalc.Mul(a, b))
-	fmt.Println("Hello")
+	
+	
+	fmt.Println("Hello") // function always start with capital EX: Println, Add
+
+	for i:=1; i<10; i++{    //for loop
+
+		fmt.Println("Hello")
+
+	}
 }
