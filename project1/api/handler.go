b68501338a8bb707e4afe4529ccc8705925a4eb9
Yashwanthreddyk2199 <yashwanth.k2199@gmail.com>
2023-11-09 15:06:52 -0500

    API

---
+++ project1/api/handler.go
@@ -0,0 +1,23 @@
+// mostly used to check if we got the correct request method
+package api
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func CreateHandler(db *sql.DB) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) { // this style of func is anoynomus function
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+			return // it is only check for correct req method so we will not return anything
+		}
+
+		if err := CreateBookLogic(db, w, r); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	}
+
+}
